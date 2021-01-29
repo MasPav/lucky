01094f5d769a23cc5829af4cf2590e2d11429595
cyprian.boateng <maspav18@gmail.com>
2021-01-29 17:44:16 +0000

    completed ui retrieving api data

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+var svg, container, vis;
 var padding = {
         top: 20,
         right: 40,
         bottom: 0,
         left: 0
     },
-    w = 500 - padding.left - padding.right,
-    h = 500 - padding.top - padding.bottom,
+    w = 600 - padding.left - padding.right,
+    h = 600 - padding.top - padding.bottom,
     r = Math.min(w, h) / 2,
     rotation = 0,
     oldrotation = 0,
@@ -13,52 +14,33 @@ var padding = {
     oldpick = [],
     color = d3.scale.category20();
 
-var data = [{
-        id: 1,
-        label: "Igor K",
-    },
-    {
-        id: 2,
-        label: "Ayeoni M",
-    },
-    {
-        id: 3,
-        label: "Ishmael N",
-    },
-    {
-        id: 4,
-        label: "Akua A",
-    },
-    {
-        id: 5,
-        label: "digital i",
-    },
-    {
-        id: 6,
-        label: "Asante T",
-    },
-    {
-        id: 7,
-        label: "Mofetoluwa",
-    },
-    {
-        id: 8,
-        label: "Godwin K",
-    },
-    {
-        id: 9,
-        label: "Nicholas m",
-    },
-    {
-        id: 10,
-        label: "mardiya z",
-    }
+var data = [];
+var initialData = [];
+getRaffleData().then(resData => {
+    data = resData.slice(0, 30);
+    initialData = JSON.stringify(data);
+    drawChart();
+    $('.spinner-container').removeClass('d-flex').hide();
+    $('.raffle-container').show('slow');
+});
 
-];
-var svg, container, vis;
-drawChart();
+function getRaffleData() {
+    return fetch('https://restcountries.eu/rest/v2/regionalbloc/au?fields=name;')
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                $('.spinner-container').hide();
+                $('.alert').show();
+            }
+        }).catch(err => {
+            $('.spinner-container').hide();
+            $('.alert').show();
+        });
+}
 
 function drawChart() {
+    d3.select('svg').remove();
     svg = d3.select('#chart')
         .append("svg")
         .data([data])
@@ -95,18 +77,53 @@ function drawChart() {
             return arc(d);
         });
 
+    // add the text
+    const fontSize = getFontSize(data);
+    arcs.append("text").attr("transform", function (d) {
+            d.innerRadius = 0;
+            d.outerRadius = r;
+            d.angle = (d.startAngle + d.endAngle) / 2;
+            return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius - 10) + ")";
+        })
+        .attr("text-anchor", "end")
+        .text(function (d, i) {
+            const name = data[i].name;
+            return name && name.length <= 15 ? name : `${name.slice(0, 12)}...`;
+        }).style({
+            'font-size': `${fontSize}px`
+        });
+    //draw spin circle
+    container.append("circle")
+        .attr("cx", 0)
+        .attr("cy", 0)
+        .attr("r", 60)
+    //make arrow
+    svg.append("g")
+        .attr("transform", "translate(" + (w + padding.left + padding.right) + "," + ((h / 2) + padding.top) + ")")
+        .append("path")
+        .attr("d", "M-" + (r * .15) + ",0L0," + (r * .05) + "L0,-" + (r * .05) + "Z")
+        .attr("class", 'arrow');
+
+    //spin text
+    container.append("text")
+        .attr("x", 0)
+        .attr("y", 15)
+        .attr("text-anchor", "middle")
+        .text("SPIN")
+        .style({
+            "font-weight": "bold",
+            "font-size": "30px",
+        });
+
     container.on("click", spin);
 }
 
 function spin(d) {
-
     container.on("click", null);
-    if (data.length <= 0) {
-        console.log("done");
-        container.on("click", null);
-        return;
-    }
-
+    d3.select('.chartholder').style({
+        'cursor': 'not-allowed'
+    });
+    $('.reset-btn').attr('disabled', true);
     var ps = 360 / data.length,
         pieslice = Math.round(1440 / data.length),
         rng = Math.floor((Math.random() * 1440) + 360);
@@ -119,55 +136,69 @@ function spin(d) {
     rotation += 90 - Math.round(ps / 2);
 
     vis.transition()
-        .duration(3000)
+        .duration(5000)
         .attrTween("transform", rotTween)
         .each("end", function () {
-            oldrotation = rotation;
-            container.on("click", spin);
-            $('.winner-name').text(data[picked].label);
-            $('.winner-wrapper').show();
+            //mark name as seen
+            d3.select(".slice:nth-child(" + (picked + 1) + ") path")
+                .attr("fill", "#111");
+            $('.winner-name').text(data[picked].name);
+            $('.winner-wrapper').show('slow');
+            $('.fireworks').show('slow');
+            setTimeout(() => {
+                container.on("click", spin);
+                d3.select('.chartholder').style({
+                    'cursor': 'pointer'
+                });
+                $('.fireworks').hide('slow');
+                $('.winner-wrapper').hide('slow');
+                $('.reset-btn').attr('disabled', false);
+                if (data.length <= 1) {
+                    console.log("done");
+                    container.on("click", null);
+                    d3.select('.chartholder').style({
+                        'cursor': 'not-allowed'
+                    });
+                    $('.raffle-ended').show('slow');
+                    return;
+                }
+                drawChart();
+            }, 5000);
             data.splice(picked, 1);
         });
 }
 
-//draw spin circle
-container.append("circle")
-    .attr("cx", 0)
-    .attr("cy", 0)
-    .attr("r", 60)
+function getFontSize(data) {
+    let size = 0;
+    if (data.length <= 2) {
+        size = 100;
+    } else
+    if (data.length <= 5) {
+        size = 150;
+    } else if (data.length <= 10) {
+        size = 200;
+    } else if (data.length <= 40) {
+        size = 500;
+    } else if (data.length <= 80) {
+        size = 700;
+    }
+    return size / data.length;
+}
+// getSome();
 
-//spin text
-container.append("text")
-    .attr("x", 0)
-    .attr("y", 15)
-    .attr("text-anchor", "middle")
-    .text("SPIN")
-    .style({
-        "font-weight": "bold",
-        "font-size": "30px",
-    });
+function onReset() {
+    $('.raffle-ended').hide('slow');
+    data = JSON.parse(initialData);
+    drawChart();
+}
+
+function getSome() {
+    fetch('https://7npi6fj537.execute-api.eu-west-1.amazonaws.com/DEV/dumpconfig').then(res => res.json()).then(data => console.log(data));
+}
 
 function rotTween(to) {
     var i = d3.interpolate(oldrotation % 360, rotation);
     return function (t) {
         return "rotate(" + i(t) + ")";
     };
-}
-
-
-function getRandomNumbers() {
-    var array = new Uint16Array(1000);
-    var scale = d3.scale.linear().range([360, 1440]).domain([0, 100000]);
-
-    if (window.hasOwnProperty("crypto") && typeof window.crypto.getRandomValues === "function") {
-        window.crypto.getRandomValues(array);
-        console.log("works");
-    } else {
-        //no support for crypto, get crappy random numbers
-        for (var i = 0; i < 1000; i++) {
-            array[i] = Math.floor(Math.random() * 100000) + 1;
-        }
-    }
-
-    return array;
 }
